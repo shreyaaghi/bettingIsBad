15bbf4b0369e7768d36a879cdf006fe1fbbd8c46
shreyaaghi <119831868+shreyaaghi@users.noreply.github.com>
2024-01-07 10:59:41 -0800

    [feat] fixed login, touched on jwt concepts

--- auth.py
+++ auth.py
@@ -1,12 +1,14 @@
 from flask import request, Response
 from util import readFile, writeToFile
 import bcrypt
+import jwt
+
 def signup():
   username = request.form['username']
-  password = request.form['password']
-  hashed = bcrypt.hashpw(b"{password}", bcrypt.gensalt())
+  password = request.form['password'].encode('utf-8') 
+  hashed = bcrypt.hashpw(password, bcrypt.gensalt())
   # FIXME; use actual password and not the string password as a byte DID I FIX THIS
-  newUser = {'username':username, 'password':hashed}
+  newUser = {'username':username, 'password':hashed.decode('utf-8')}
   users = readFile('users.json')
   for user in users:
     if user.get('username') == username:
@@ -15,25 +17,23 @@ def signup():
   writeToFile('users.json', users)
   return str(newUser)
 # return jwt later (WHAT IS JWT)
-# how to do login?
 
 def login():
     username = request.form['username']
     password = request.form['password'].encode('utf-8')  # Convert to bytes
-
     users = readFile('users.json')
     user_found = None
     for user in users:
         if user.get('username') == username:
             user_found = user
             break
-
-    if user_found:
-        hashed_password = user_found.get('password').encode('utf-8') 
-        #retrieves the stored hashed password
-        if bcrypt.checkpw(password, hashed_password):
-            return Response('Login successful', 200)
-        else:
-            return Response('Incorrect password', 401)
+    if user_found is None:
+      return Response('User not found', 404) 
     else:
-        return Response('User not found', 404)
+      hashed_password = user_found.get('password').encode('utf-8')
+        #retrieves the stored hashed password
+      if bcrypt.checkpw(password, hashed_password):
+        token = jwt.encode({'user':user_found},'jwt/secret', algorithm = 'HS256')
+        return Response(token, 200)
+      else:
+        return Response('Incorrect password', 401)
