3b8147c903a2a5b8b1c84e276221610aaa20bd96
shreyaaghi <119831868+shreyaaghi@users.noreply.github.com>
2024-01-07 09:53:17 -0800

    [feat] forgot to push, tried to add login

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response
 from auth import signup
+from auth import login
 
 app = Flask(__name__)
 
@@ -10,6 +11,10 @@ def index():
 @app.route('/signup', methods=['POST'])
 def signup_route():
     return signup()
+
+@app.route('/login', methods=['POST'])
+def login_route():
+    return login()
 
 def run():
     app.run(host='0.0.0.0', port=81)
--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ def signup():
   username = request.form['username']
   password = request.form['password']
   hashed = bcrypt.hashpw(b"{password}", bcrypt.gensalt())
-  # FIXME; use actual password and not the string password as a byte
+  # FIXME; use actual password and not the string password as a byte DID I FIX THIS
   newUser = {'username':username, 'password':hashed}
   users = readFile('users.json')
   for user in users:
@@ -16,3 +16,24 @@ def signup():
   return str(newUser)
 # return jwt later (WHAT IS JWT)
 # how to do login?
+
+def login():
+    username = request.form['username']
+    password = request.form['password'].encode('utf-8')  # Convert to bytes
+
+    users = readFile('users.json')
+    user_found = None
+    for user in users:
+        if user.get('username') == username:
+            user_found = user
+            break
+
+    if user_found:
+        hashed_password = user_found.get('password').encode('utf-8') 
+        #retrieves the stored hashed password
+        if bcrypt.checkpw(password, hashed_password):
+            return Response('Login successful', 200)
+        else:
+            return Response('Incorrect password', 401)
+    else:
+        return Response('User not found', 404)
